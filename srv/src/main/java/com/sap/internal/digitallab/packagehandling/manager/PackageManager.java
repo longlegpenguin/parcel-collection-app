1ac4fea3d2fdc486d8e30caa127fa576d2a26e38
Li Zhang <li.zhang09@sap.com>
2023-10-06 21:10:57 +0200

    Implemented related package updated upon deletion of delivery company

--- srv/src/main/java/com/sap/internal/digitallab/packagehandling/manager/PackageManager.java
+++ srv/src/main/java/com/sap/internal/digitallab/packagehandling/manager/PackageManager.java
@@ -1,5 +1,50 @@
 package com.sap.internal.digitallab.packagehandling.manager;
 
-public class PackageManager {
+import cds.gen.com.sap.internal.digitallab.packagehandling.core.Package;
+import com.sap.cds.Result;
+import com.sap.internal.digitallab.packagehandling.repository.DeliveryCompanyRepository;
+import com.sap.internal.digitallab.packagehandling.repository.PackageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class PackageManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger("PackageManager_Logger");
+    private final DeliveryCompanyRepository companyRepository;
+    private final PackageRepository packageRepository;
+
+    @Autowired
+    public PackageManager(DeliveryCompanyRepository companyRepository, PackageRepository packageRepository) {
+        this.companyRepository = companyRepository;
+        this.packageRepository = packageRepository;
+    }
+
+    /**
+     * Goes through all packages.
+     * Updates reference to a non-existing company to null.
+     */
+    public void updateDeletedDeliveryCompany() {
+        Result rowsOfPackages = packageRepository.selectAllPackages();
+        rowsOfPackages.stream().forEach(p -> {
+            if (!checkCompanyExistById(
+                    p.get(Package.DELIVERY_COMPANY_ID).toString())) {
+                LOGGER.info("Updated Package {} 's company to null", p);
+                packageRepository.removeDeliveryCompanyById(
+                        p.get(Package.ID).toString());
+            }
+        });
+    }
+
+    /**
+     * Check if a given company exists in the database.
+     *
+     * @param companyId company UUID to check.
+     * @return true if exists.
+     */
+    private boolean checkCompanyExistById(String companyId) {
+        Result rowsOfCompanies = companyRepository.selectById(companyId);
+        return (rowsOfCompanies.stream().count() == 1);
+    }
 }
--- srv/src/main/java/com/sap/internal/digitallab/packagehandling/repository/DeliveryCompanyRepository.java
+++ srv/src/main/java/com/sap/internal/digitallab/packagehandling/repository/DeliveryCompanyRepository.java
@@ -1,4 +1,35 @@
 package com.sap.internal.digitallab.packagehandling.repository;
 
+import cds.gen.com.sap.internal.digitallab.packagehandling.core.DeliveryCompany_;
+import com.sap.cds.Result;
+import com.sap.cds.ql.Select;
+import com.sap.cds.ql.cqn.CqnSelect;
+import com.sap.cds.services.persistence.PersistenceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DeliveryCompanyRepository {
+    private static final Logger LOGGER = LoggerFactory.getLogger("DeliveryCompanyRepository_Logger");
+    private final PersistenceService db;
+
+    @Autowired
+    public DeliveryCompanyRepository(PersistenceService db) {
+        this.db = db;
+    }
+
+    /**
+     * SELECT * FROM delivery_company WHERE id = $id;
+     *
+     * @param id company UUID to check.
+     * @return resulting rows
+     */
+    public Result selectById(String id) {
+        CqnSelect select = Select
+                .from(DeliveryCompany_.class)
+                .byId(id);
+        return db.run(select);
+    }
 }
