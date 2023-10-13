3f1c03216c48e258c57c3a37fc01263a990eba7a
Li Zhang <li.zhang09@sap.com>
2023-10-13 08:05:29 +0000

    Added handler skeleton of registration service

--- srv/src/main/java/com/sap/internal/digitallab/packagehandling/handler/RegistrationServiceHandler.java
+++ srv/src/main/java/com/sap/internal/digitallab/packagehandling/handler/RegistrationServiceHandler.java
@@ -1,4 +1,23 @@
 package com.sap.internal.digitallab.packagehandling.handler;
 
-public class RegistrationServiceHandler {
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.services.cds.CqnService;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.handler.annotations.HandlerOrder;
+
+import cds.gen.com.sap.internal.digitallab.packagehandling.service.registrationservice.RegistrationService_;
+import cds.gen.com.sap.internal.digitallab.packagehandling.service.registrationservice.Package;
+
+@Component
+@ServiceName(RegistrationService_.CDS_NAME)
+public class RegistrationServiceHandler implements EventHandler {
+
+    @On(event = { CqnService.EVENT_CREATE })
+    @HandlerOrder(HandlerOrder.EARLY)
+    public void onCreate(Package pack) {
+        pack.setStatusCode("new");
+    }
 }
