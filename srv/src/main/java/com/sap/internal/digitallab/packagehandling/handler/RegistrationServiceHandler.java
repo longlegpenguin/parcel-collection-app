6967ab63c6829f0e8ac47136feb7bb3f76c9e7bf
Li Zhang <li.zhang09@sap.com>
2023-10-13 08:18:58 +0000

    C and its test request for registration service

--- srv/src/main/java/com/sap/internal/digitallab/packagehandling/handler/RegistrationServiceHandler.java
+++ srv/src/main/java/com/sap/internal/digitallab/packagehandling/handler/RegistrationServiceHandler.java
@@ -1,11 +1,13 @@
 package com.sap.internal.digitallab.packagehandling.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
-import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.HandlerOrder;
 
 import cds.gen.com.sap.internal.digitallab.packagehandling.service.registrationservice.RegistrationService_;
@@ -14,10 +16,12 @@ import cds.gen.com.sap.internal.digitallab.packagehandling.service.registrations
 @Component
 @ServiceName(RegistrationService_.CDS_NAME)
 public class RegistrationServiceHandler implements EventHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger("StorageServiceHandler_logger");
 
-    @On(event = { CqnService.EVENT_CREATE })
+    @Before(event = { CqnService.EVENT_CREATE })
     @HandlerOrder(HandlerOrder.EARLY)
-    public void onCreate(Package pack) {
+    public void beforeCreate(Package pack) {
         pack.setStatusCode("new");
+        LOGGER.info("Creating package: {}", pack);
     }
 }
