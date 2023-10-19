7c548e68f625b4f5d2f06197ea2a9dd0afd1072b
Li Zhang <li.zhang09@sap.com>
2023-10-19 15:14:29 +0200

    Generated empty test class for managers.

---
+++ srv/src/test/java/com/sap/internal/digitallab/packagehandling/manager/PackageManagerTest.java
@@ -0,0 +1,50 @@
+package com.sap.internal.digitallab.packagehandling.manager;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PackageManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void evalDeleteAc() {
+    }
+
+    @Test
+    void evalConfirmAc() {
+    }
+
+    @Test
+    void evalPickupAc() {
+    }
+
+    @Test
+    void confirmPackage() {
+    }
+
+    @Test
+    void pickupPackage() {
+    }
+
+    @Test
+    void getPackageWithId() {
+    }
+
+    @Test
+    void updateDeletedDeliveryCompany() {
+    }
+
+    @Test
+    void getFilterUserAndConfirmedStatusCqn() {
+    }
+}
