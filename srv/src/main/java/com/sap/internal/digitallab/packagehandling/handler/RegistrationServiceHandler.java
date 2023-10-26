91308f24ab59a8d9f3054f1110a6ec5e13b92d7f
Li Zhang <li.zhang09@sap.com>
2023-10-26 17:53:15 +0200

    Updated RegistrationServiceHandler, autofill receptionist when role is Receptionist.

--- srv/src/main/java/com/sap/internal/digitallab/packagehandling/handler/RegistrationServiceHandler.java
+++ srv/src/main/java/com/sap/internal/digitallab/packagehandling/handler/RegistrationServiceHandler.java
@@ -1,5 +1,6 @@
 package com.sap.internal.digitallab.packagehandling.handler;
 
+import com.sap.cds.services.cds.CdsCreateEventContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,8 @@ import com.sap.cds.services.handler.annotations.HandlerOrder;
 import cds.gen.com.sap.internal.digitallab.packagehandling.service.registrationservice.RegistrationService_;
 import cds.gen.com.sap.internal.digitallab.packagehandling.service.registrationservice.Package;
 
+import java.util.Set;
+
 @Component
 @ServiceName(RegistrationService_.CDS_NAME)
 public class RegistrationServiceHandler implements EventHandler {
@@ -20,8 +23,13 @@ public class RegistrationServiceHandler implements EventHandler {
 
     @Before(event = { CqnService.EVENT_CREATE })
     @HandlerOrder(HandlerOrder.EARLY)
-    public void beforeCreate(Package pack) {
+    public void beforeCreate(Package pack, CdsCreateEventContext context) {
         pack.setStatusCode("new");
+        Set<String> userRoles = context.getUserInfo().getRoles();
+        String userId = context.getUserInfo().getId();
+        if (userRoles.contains("Receptionist")) {
+            pack.setReceptionist(userId);
+        }
         LOGGER.info("Creating package: {}", pack);
     }
 }
